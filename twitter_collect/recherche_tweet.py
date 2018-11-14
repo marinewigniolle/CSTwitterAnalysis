3edde26837f52fdfdff66f0734721709ba358cad
marine.wigniolle@student-cs.fr <Benodet29!>
2018-11-14 16:00:07 +0100

    Travail après-midi

--- twitter_collect/recherche_tweet.py
+++ twitter_collect/recherche_tweet.py
@@ -1,4 +1,5 @@
 from twitter_connection_setup import twitter_setup
+import tweepy
 
 def collect():
     connexion = twitter_setup()
@@ -6,6 +7,40 @@ def collect():
     for tweet in tweets:
         print(tweet.text)
 
-collect()
+
+def collect_by_user(user_id):
+    connexion = twitter_setup()
+    statuses = connexion.user_timeline(id = user_id, count = 200)
+    for status in statuses:
+        print(status.text)
+    return statuses
+
+
+from tweepy.streaming import StreamListener
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        if  str(status) == "420":
+            print(status)
+            print("You exceed a limited number of attempts to connect to the streaming API")
+            return False
+        else:
+            return True
+
+
+
+
+def collect_by_streaming():
+
+    connexion = twitter_setup()
+    listener = StdOutListener()
+    stream= tweepy.Stream(auth = connexion.auth, listener=listener)
+    stream.filter(track=['Emmanuel Macron'])
+
 
 
