1fec904a90ae5a145fef61997dedc679a4b166cb
marine.wigniolle@student-cs.fr <Benodet29!>
2018-11-16 15:59:09 +0100

    Fonctionnalité 9

--- Spring3/test_tweet_extraire.py
+++ Spring3/test_tweet_extraire.py
@@ -2,4 +2,4 @@ import pytest
 from tweet_extraire import recuperer_mots_tweet
 
 def test_tweet_extraire():
-    assert tweet_extraire() != []
+    assert recuperer_mots_tweet() != []
---
+++ Spring3/test_tweet_sentiment.py
@@ -0,0 +1,7 @@
+from tweet_sentiment import analyse_opinion_candidate
+
+def test_analyse_opinion_candidate():
+    assert type(analyse_opinion_candidate())==float
+
+
+
